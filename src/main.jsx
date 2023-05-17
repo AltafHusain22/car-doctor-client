37af289d4d2b85aa534292703e8b7cf454369201
AltafHusain22 <fiverr.contactme@gmail.com>
2023-05-17 08:51:39 +0600

    menu added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./LayOuts/Main/MainPage.jsx";
+import Home from "./pages/Home/components/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage></MainPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
